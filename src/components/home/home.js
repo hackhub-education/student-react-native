f59bbe10046ba756f0cc482bf15896b1c90e5cf9
m1chaelchen <cyjdyj@gmail.com>
2018-08-07 14:55:29 -0700

    add formik

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,80 +1,83 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Button,
-  TextInput
-} from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+import { withFormik } from 'formik';
+import * as Yup from 'yup';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center"
   },
   inputRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 10
   },
   inputField: {
     marginLeft: 10,
     padding: 5,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderWidth: 0.5,
-    borderColor: 'grey',
+    borderColor: "grey",
     width: 150
   }
 });
 
-class Home extends Component {
-  state={
+const enhancer = withFormik({
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required('Name is required !'),
+    email: Yup.string().email('Must be a valid email').required('Email is required !'),
+    phone: Yup.string().required('Phone is required !')
+  }),
+  mapPropsToValues: () => ({
     name: '',
     email: '',
     phone: ''
+  }),
+  handleSubmit: (values, { props }) => {
+    props.navigation.navigate("Card", { data: values })
   }
+});
 
-  render() {
-    const { name, email, phone } = this.state;
+const Home = ({ values: { name, email, phone }, navigation, handleChange, handleSubmit, errors }) => (
+  <View style={styles.container}>
+    <Text>Welcome, {navigation.getParam("username", "")} !</Text>
+    <View style={styles.inputRow}>
+      <Text>Name</Text>
+      <TextInput
+        style={styles.inputField}
+        placeholder="type name..."
+        value={name}
+        onChangeText={handleChange('name')}
+      />
+    </View>
+    {errors.name && <Text>{errors.name}</Text>}
+    <View style={styles.inputRow}>
+      <Text>Email</Text>
+      <TextInput
+        style={styles.inputField}
+        placeholder="type email..."
+        value={email}
+        onChangeText={handleChange('email')}
+      />
+    </View>
+    {errors.email && <Text>{errors.email}</Text>}
+    <View style={styles.inputRow}>
+      <Text>Cell Phone</Text>
+      <TextInput
+        style={styles.inputField}
+        placeholder="type phone..."
+        value={phone}
+        onChangeText={handleChange('phone')}
+      />
+    </View>
+    {errors.phone && <Text>{errors.phone}</Text>}
+    <Button
+      title="Generate"
+      onPress={handleSubmit}
+    />
+  </View>
+);
 
-    return (
-      <View style={styles.container}>
-        <Text>Welcome, {this.props.navigation.getParam('username', '')} !</Text>
-        <View style={styles.inputRow}>
-          <Text>Name</Text>
-          <TextInput
-            style={styles.inputField}
-            placeholder="type name..."
-            value={name}
-            onChangeText={(name) => this.setState({ name })}
-          />
-        </View>
-        <View style={styles.inputRow}>
-          <Text>Email</Text>
-          <TextInput
-            style={styles.inputField}
-            placeholder="type email..."
-            value={email}
-            onChangeText={(email) => this.setState({ email })}
-          />
-        </View>
-        <View style={styles.inputRow}>
-          <Text>Cell Phone</Text>
-          <TextInput
-            style={styles.inputField}
-            placeholder="type phone..."
-            value={phone}
-            onChangeText={(phone) => this.setState({ phone })}
-          />
-        </View>
-        <Button
-          title="Generate"
-          onPress={() => this.props.navigation.navigate('Card', { data: this.state })}
-        />
-      </View>
-    );
-  }
-};
-
-export default Home;
+export default enhancer(Home);
